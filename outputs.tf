a82838407cf7a9f3d4ddae69db032601730d2ff7
Yurii <yurij.kenu@gmail.com>
2025-11-26 19:24:42 +0100

    added outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "subnet_id" {
   description = "ID of the VPC subnet, deployed by the module"
-  value       = ""
+  value       = aws_subnet.grafana_subnet.id
 }
 
 output "security_group_id" {
   description = "ID of the security group, deployed by the module"
-  value       = ""
+  value       = aws_security_group.grafana_sg.id
 }
